64ea01fd8e35d51c94e8489681191b85f56331ec
Caleb Sylvest <caleb.sylvest@gmail.com>
2015-02-14 12:37:13 -0600

    add smooth scroll to hash points.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,6 +10,22 @@
 			$(this).siblings('.email-signup-submit').addClass("show"); // add class to submit button
 			$(this).attr('placeholder','Email Address...'); // change text in input field
 		});
+
+		// smooth scroll to hash points
+		$(function() {
+			$('a[href*=#]:not([href=#])').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top
+						}, 1000);
+						return false;
+					}
+				}
+			});
+		});
 	
 	});
 
